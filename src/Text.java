30e2ed604102a08089aba734fbc4b18a1d9b467f
Voronovskyy <48327247+Voronovskyy@users.noreply.github.com>
2019-04-11 18:11:34 +0300

    Update Text.java

--- src/Text.java
+++ src/Text.java
@@ -1,29 +1,24 @@
 public class Text {
     public void greet(){
-        System.out.println("Hello Dear Customer!" + " Welcome to our shop!" + "\nPlease type in the name of the category from the list : \n");
-        System.out.println("╔=======================================╗");
-        System.out.println("║ FOOD \tDRINK\tHOUSEHOLD\tELECTRONICS ║");
-        System.out.println("╚=======================================╝");
+        System.out.println("Hello Dear Customer!" + " Welcome to our shop!");
+        System.out.println("If you want buy something, please type in the name of the category from the list :\n");
+        System.out.println("=======================================");
+        System.out.println("FOOD\tDRINK\tHOUSEHOLD\tELECTRONICS");
+        System.out.println("=======================================\n");
+        System.out.println("Type \"Basket\" to see you purchase list." );
+        System.out.println("Type \"Exit\" finish you shopping.");
+        System.out.println("To remove an Item from basket type \"-\", than press \"Enter\" and type the name of the Item.\n" );
     }
     public void menuList(){
-        System.out.println("╔=======================================╗");
-        System.out.println("║ FOOD \tDRINK\tHOUSEHOLD\tELECTRONICS ║");
-        System.out.println("╚=======================================╝");
-    }
-    public void header(){
-        System.out.println("╔======================╤============╤=====╗");
-        System.out.println("║         Name         │   Price    │  -  ║");
-        System.out.println("╠======================╪============╪=====╣");
-    }
-    public void footer(){
-        System.out.println("╚======================╧============╧=====╝");
+        System.out.println("=======================================");
+        System.out.println("FOOD\tDRINK\tHOUSEHOLD\tELECTRONICS");
+        System.out.println("=======================================");
     }
     public void hint(){
-        System.out.println("\n"+"Main menu press \"M\" " );
-        System.out.println("To add an Item press \"+\", than press \"Enter\" and type the name of the Item. " );
-        System.out.println("To remove an Item press \"-\", than press \"Enter\" and type the name of the Item. " );
-        System.out.println("Press \"Basket\" to see you purchase list. " );
-        System.out.println("Press \"Exit\" finish you shopping.");
+        System.out.println("\n"+"For come back to main menu type \"M\" " );
+        System.out.println("To add an Item type \"+\", than press \"Enter\" and type the name of the Item. " );
+        System.out.println("Type \"Basket\" to see your purchase list. " );
+        System.out.println("Type \"Exit\" finish you shopping.");
     }
     public void bye(){
         System.out.println("Bye! Thank you for shopping with us!");
